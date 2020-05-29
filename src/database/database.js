ca661685482366f957926968a1d7387100598126
Angel Feü <angel.a.feu@gmail.com>
2020-05-29 16:58:55 -0300

    establecer coneccion heroku db

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize';
 
 export const sequelize = new Sequelize(
-    'postgres',
-    'postgres',
-    '122334',
+    'dasqfajvt86bti',
+    'olmrkmpylmmgvj',
+    '98b074723244f296754cd7e140efb550e110424732bcc7c93e64bf11332408f8',
     {
-        host: 'localhost',
+        host: 'ec2-52-202-22-140.compute-1.amazonaws.com',
         dialect: 'postgres',
         poll: {
             max: 5,
