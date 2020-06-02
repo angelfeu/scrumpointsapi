882e602031188554ede2129c7320cfe925deaba8
feu.angel <angel.feu.ext@level3.com>
2020-06-01 21:27:06 -0300

    prueba babel cli

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize';
 
 export const sequelize = new Sequelize(
-    'dasqfajvt86bti',
-    'olmrkmpylmmgvj',
-    '98b074723244f296754cd7e140efb550e110424732bcc7c93e64bf11332408f8',
+    'postgres',
+    'postgres',
+    '122334',
     {
-        host: 'ec2-52-202-22-140.compute-1.amazonaws.com',
+        host: 'localhost',
         dialect: 'postgres',
         poll: {
             max: 5,
